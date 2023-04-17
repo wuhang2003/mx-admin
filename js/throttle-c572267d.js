7ef3520a39e5e16e58e863cbc8af01cd46576af6
Innei <Innei@users.noreply.github.com>
2023-04-17 14:14:06 +0000

    deploy: c6787c3a406e2d356bfa2008a64fb5460ee0cb24

---
+++ js/throttle-c572267d.js
@@ -0,0 +1,1 @@
+import{bn as n,ah as l}from"./index-60eb200c.js";var g="Expected a function";function f(a,t,r){var e=!0,i=!0;if(typeof a!="function")throw new TypeError(g);return n(r)&&(e="leading"in r?!!r.leading:e,i="trailing"in r?!!r.trailing:i),l(a,t,{leading:e,maxWait:t,trailing:i})}export{f as t};
---
+++ js/use-react-14ded14b.js
@@ -0,0 +1,1 @@
+import{g as t,x as e}from"./index-60eb200c.js";const u=n=>{t(()=>{const o=n();e(()=>{o&&typeof o=="function"&&o()})})};export{u};
