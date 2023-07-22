fd863a01b397b32d56c02c957174365b707cc003
Innei <Innei@users.noreply.github.com>
2023-07-22 05:17:30 +0000

    deploy: 3a83fe1ec70d837c88502c6f69c257f8dd0ea50d

---
+++ js/two-col-ad02f836.js
@@ -0,0 +1,1 @@
+import{d as e,h as t,c9 as a}from"./index-2ccee8cf.js";const d=e({setup(r,{slots:o}){return()=>t(a,{cols:"36 1:12 1024:36 1600:36",xGap:24},{default:()=>[o?.default?.()]})}});export{d as T};
